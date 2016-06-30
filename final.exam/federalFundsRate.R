cd60df812f2a71f4132f42d04f57ce75a998d079
Diego Dell'Era <ddellera@socialmetrix.com>
2016-06-30 14:37:09 -0300

    final wip

--- final.exam/federalFundsRate.R
+++ final.exam/federalFundsRate.R
@@ -55,7 +55,7 @@ as.numeric(performance(model.5.roc.test, "auc")@y.values)
 ## respuesta oficial: The AUC is the proportion of time the model can differentiate between a randomly selected true positive and true negative.
 
 model.5.test.perf = performance(model.5.roc.test, "tpr", "fpr") # true positive rate, false positive rate
-plot(model.5.test.perf)
+plot(model.5.test.perf, colorize=TRUE)
 
 ## A model with threshold 0 predicts 1 for all observations, yielding a 100% true positive rate and a 100% false positive rate. => logistic regression threshold = 0
 
